ead41000cb89a66429a3b7b5f466b641dc66f09b
johhny47 <mdjohnny063@gmail.com>
2024-09-19 11:33:07 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.getElementById('Login-button').addEventListener('click', function(event){
+    event.preventDefault();
+    
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pin = document.getElementById('pin').value;
+   
+    if(phoneNumber === '01953395475' && pin==='1234'){
+        console.log("login successful");
+        window.location.href='./home.html'
+    }
+    else{
+        console.log("login failed");
+    }
+})
